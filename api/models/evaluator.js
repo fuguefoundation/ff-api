3c535f04e4f83618204c92108c7e48eab0dbba43
Fugue Foundation <admin@fuguefoundation.org>
2020-05-10 23:55:41 -0400

    added _ back to id model

--- api/models/evaluator.js
+++ api/models/evaluator.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const evaluatorSchema = mongoose.Schema({
-	id: mongoose.Schema.Types.ObjectId,
+	_id: mongoose.Schema.Types.ObjectId,
 	name: {type: String, required: true},
     url: {type: String, required: true},
 	image: {type: String, required: true},
--- api/models/nonprofit.js
+++ api/models/nonprofit.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const nonprofitSchema = mongoose.Schema({
-	id: mongoose.Schema.Types.ObjectId,
+	_id: mongoose.Schema.Types.ObjectId,
 	evaluatorId: {
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'Evaluator',
