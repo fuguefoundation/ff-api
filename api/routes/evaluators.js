51b215fcf0c5223d10705fe50e1653157c8658a5
Fugue Foundation <admin@fuguefoundation.org>
2020-05-10 22:13:11 -0400

    edits to id response object

--- api/routes/evaluators.js
+++ api/routes/evaluators.js
@@ -5,129 +5,158 @@ const mongoose = require('mongoose');
 const Evaluator = require('../models/evaluator');
 
 router.get('/', (req, res, next) => {
-	Evaluator.find()
-	.select('_id name url logo image focus short_desc desc')
-	.exec()
-	.then(docs => {
-		const response = docs.map(doc => {
-				return {
-					_id: doc._id,
-					name: doc.name,
-					url: doc.url,
-					logo: doc.logo,
+    Evaluator.find()
+        .select('_id name url logo image focus short_desc desc')
+        .exec()
+        .then(docs => {
+            const response = docs.map(doc => {
+                return {
+                    _id: doc._id,
+                    name: doc.name,
+                    url: doc.url,
+                    logo: doc.logo,
                     image: doc.image,
                     focus: doc.focus,
-					short_desc: doc.short_desc,
-					desc: doc.desc,
-					request: {
-						type: 'GET',
-						url: 'api/v0/evaluators/' + doc._id
-					}
-				}
-			});
-		res.status(200).json(response);
-	}).catch(err => {
-		console.log(err);
-		res.status(500).json({error: err});
-	});
+                    short_desc: doc.short_desc,
+                    desc: doc.desc,
+                    request: {
+                        type: 'GET',
+                        url: 'api/v0/evaluators/' + doc._id
+                    }
+                }
+            });
+            res.status(200).json(response);
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.post('/', (req, res, next) => {
-	console.log(req.file);
-	const evaluator = new Evaluator({
-		_id: new mongoose.Types.ObjectId(),
-		name: req.body.name,
-		url: req.body.url,
-		logo: req.body.logo,
+    console.log(req.file);
+    const evaluator = new Evaluator({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        url: req.body.url,
+        logo: req.body.logo,
         image: req.body.image,
         focus: req.body.focus,
-		short_desc: req.body.short_desc,
-		desc: req.body.desc
-	});
+        short_desc: req.body.short_desc,
+        desc: req.body.desc
+    });
 
-	evaluator.save().then(result => {
-		console.log(result);
-		res.status(201).json({
-			message: "New evaluator created",
-			createdEvaluator: {
-				_id: result._id,
-				name: result.name,
-				request: {
-					type: 'GET',
-					url: '/api/v0/evaluators/' + result._id
-				}
-			}
-		});
-	}).catch(err => {
-		console.log(err);
-		res.status(500).json({
-			error: err
-		});
-	});
+    evaluator.save().then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: "New evaluator created",
+            createdEvaluator: {
+                _id: result._id,
+                name: result.name,
+                request: {
+                    type: 'GET',
+                    url: '/api/v0/evaluators/' + result._id
+                }
+            }
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
 });
 
 router.get('/:evaluatorId', (req, res, next) => {
-	const id = req.params.evaluatorId;
-	Evaluator.findById(id)
-		.select('_id name url logo image focus short_desc desc')
-		.exec()
-		.then(doc => {
-			if (doc){
-				res.status(200).json({
-					doc,
-					request: {
-						type: 'GET',
-						description: 'Get all evaluators',
-						url: 'api/v0/evaluators'
-					}
-				});
-			} else {
-				res.status(404).json({message: "No valid entry for provided ID"})
-			}
-		}).catch( err => {
-			console.log(err);
-			res.status(500).json({error: err});
-	});
+    const id = req.params.evaluatorId;
+    Evaluator.findById(id)
+        .select('_id name url logo image focus short_desc desc')
+        .exec()
+        .then(doc => {
+            if (doc) {
+                const response = {
+                    _id: doc._id,
+                    name: doc.name,
+                    url: doc.url,
+                    logo: doc.logo,
+                    image: doc.image,
+                    focus: doc.focus,
+                    short_desc: doc.short_desc,
+                    desc: doc.desc,
+                    request: {
+                        type: 'GET',
+                        url: 'api/v0/evaluators'
+                    }
+                };
+                res.status(200).json(response);
+            } else {
+                res.status(404).json({
+                    message: "No valid entry for provided ID"
+                })
+            }
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.patch('/:evaluatorId', (req, res, next) => {
-	const id = req.params.evaluatorId;
-	const updateOps = {};
-	for (const ops of req.body){
-		updateOps[ops.propName] = ops.value;
-	}
-	Evaluator.update({_id: id}, { $set: updateOps }).exec().then(result => {
-		res.status(200).json({
-			message: 'Evaluator updated',
-			request: {
-				type: 'GET',
-				url: 'api/v0/evaluators/' + id
-			}
-		});
-	}).catch( err => {
-		console.log(err);
-		res.status(500).json({error: err});
-	});
+    const id = req.params.evaluatorId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Evaluator.update({
+        _id: id
+    }, {
+        $set: updateOps
+    }).exec().then(result => {
+        res.status(200).json({
+            message: 'Evaluator updated',
+            request: {
+                type: 'GET',
+                url: 'api/v0/evaluators/' + id
+            }
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
 });
 
 router.delete('/:evaluatorId', (req, res, next) => {
-	const id = req.params.evaluatorId;
-	Evaluator.remove({_id: id})
-	.exec()
-	.then(result =>{
-		res.status(200).json({
-			message: 'Evaluator deleted',
-			request: {
-				type: 'POST',
-				url: 'api/v0/evaluators',
-                body: { name: 'String', url: 'String', image: 'String', 
-                logo: 'String', desc: 'String', short_desc: 'String'}
-			}
-		});
-	}).catch(err =>{
-		console.log(err);
-		res.status(500).json({error: err})
-	});
+    const id = req.params.evaluatorId;
+    Evaluator.remove({
+            _id: id
+        })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'Evaluator deleted',
+                request: {
+                    type: 'POST',
+                    url: 'api/v0/evaluators',
+                    body: {
+                        name: 'String',
+                        url: 'String',
+                        image: 'String',
+                        logo: 'String',
+                        desc: 'String',
+                        short_desc: 'String'
+                    }
+                }
+            });
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 
 module.exports = router;
--- api/routes/nonprofits.js
+++ api/routes/nonprofits.js
@@ -6,48 +6,50 @@ const Nonprofit = require('../models/nonprofit');
 const Evaluator = require('../models/evaluator');
 
 router.get('/', (req, res, next) => {
-	Nonprofit.find()
-		.select('_id name url address logo image short_desc desc evaluatorId stats')
-		.populate('evaluatorId', 'name')
-		.exec()
-		.then(docs => {
-			const response = docs.map(doc => {
-					return {
-						_id: doc._id,
-                        name: doc.name,
-                        url: doc.url,
-                        address: doc.address,
-                        logo: doc.logo,
-                        image: doc.image,
-                        short_desc: doc.short_desc,
-                        desc: doc.desc,
-						evaluatorId: doc.evaluatorId,
-						stats: doc.stats,
-						request: {
-							type: 'GET',
-							url: 'api/v0/nonprofits/' + doc._id
-						}
-					}
-				});
-			res.status(200).json(response);
-		}).catch(err => {
-			console.log(err);
-			res.status(500).json({error: err});
-	});
+    Nonprofit.find()
+        .select('_id name url address logo image short_desc desc evaluatorId stats')
+        .populate('evaluatorId', 'name')
+        .exec()
+        .then(docs => {
+            const response = docs.map(doc => {
+                return {
+                    _id: doc._id,
+                    name: doc.name,
+                    url: doc.url,
+                    address: doc.address,
+                    logo: doc.logo,
+                    image: doc.image,
+                    short_desc: doc.short_desc,
+                    desc: doc.desc,
+                    evaluatorId: doc.evaluatorId,
+                    stats: doc.stats,
+                    request: {
+                        type: 'GET',
+                        url: 'api/v0/nonprofits/' + doc._id
+                    }
+                }
+            });
+            res.status(200).json(response);
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.post('/', (req, res, next) => {
     //make sure evaluator exists
     console.log(req.body);
-	Evaluator.findById(req.body.evaluatorId)
-		.then(evaluatorId => {
-			if(!evaluatorId){
-				return res.status(404).json({
-					message: "Evaluator not found. Check evaluator ID"
-				});
-			}
-			const nonprofit = new Nonprofit({
-				_id: new mongoose.Types.ObjectId(),
+    Evaluator.findById(req.body.evaluatorId)
+        .then(evaluatorId => {
+            if (!evaluatorId) {
+                return res.status(404).json({
+                    message: "Evaluator not found. Check evaluator ID"
+                });
+            }
+            const nonprofit = new Nonprofit({
+                _id: new mongoose.Types.ObjectId(),
                 name: req.body.name,
                 url: req.body.url,
                 address: req.body.address,
@@ -55,97 +57,132 @@ router.post('/', (req, res, next) => {
                 image: req.body.image,
                 short_desc: req.body.short_desc,
                 desc: req.body.desc,
-				evaluatorId: req.body.evaluatorId,
+                evaluatorId: req.body.evaluatorId,
                 stats: req.body.stats,
-			});
-			return nonprofit.save()
-		})
-		.then(result =>{
-			res.status(201).json({
-				message: "Nonprofit created",
-				createdNonprofit: {
-					name: result.name,                  
-					_id: result._id,
-					request: {
-						type: 'GET',
-						url: 'api/v0/nonprofits/' + result._id
-					}
-				}
-			});
-		})
-		.catch(err => {
-			console.log(err);
-			res.status(500).json({
-				message: "Evaluator not found",
-				error: err
-			})
-		});
+            });
+            return nonprofit.save()
+        })
+        .then(result => {
+            res.status(201).json({
+                message: "Nonprofit created",
+                createdNonprofit: {
+                    name: result.name,
+                    _id: result._id,
+                    request: {
+                        type: 'GET',
+                        url: 'api/v0/nonprofits/' + result._id
+                    }
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: "Evaluator not found",
+                error: err
+            })
+        });
 });
 
 router.get('/:nonprofitId', (req, res, next) => {
-	const id = req.params.nonprofitId;
-	Nonprofit.findById(id)
-		.select('_id name url address logo image short_desc desc evaluatorId stats')
-		.populate('evaluatorId')
-		.exec()
-		.then(doc => {
-			if (doc){
-				res.status(200).json({
-					doc,
-					request: {
-						type: 'GET',
-						description: 'Get all nonprofits',
-						url: 'api/v0/nonprofits'
-					}
-				});
-			} else {
-				res.status(404).json({message: "No valid entry for provided ID"})
-			}
-		}).catch( err => {
-			console.log(err);
-			res.status(500).json({error: err});
-	});
+    const id = req.params.nonprofitId;
+    Nonprofit.findById(id)
+        .select('_id name url address logo image short_desc desc evaluatorId stats')
+        .populate('evaluatorId')
+        .exec()
+        .then(doc => {
+            if (doc) {
+                const response = {
+                    _id: doc._id,
+                    name: doc.name,
+                    url: doc.url,
+                    address: doc.address,
+                    logo: doc.logo,
+                    image: doc.image,
+                    short_desc: doc.short_desc,
+                    desc: doc.desc,
+                    evaluatorId: doc.evaluatorId,
+                    stats: doc.stats,
+                    request: {
+                        type: 'GET',
+                        url: 'api/v0/nonprofits'
+                    }
+                };
+                res.status(200).json(response);
+            } else {
+                res.status(404).json({
+                    message: "No valid entry for provided ID"
+                })
+            }
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.patch('/:nonprofitId', (req, res, next) => {
-	const id = req.params.nonprofitId;
-	const updateOps = {};
-	for (const ops of req.body){
-		updateOps[ops.propName] = ops.value;
-	}
-	Nonprofit.update({_id: id}, { $set: updateOps }).exec().then(result => {
-		res.status(200).json({
-			message: 'Nonprofit updated',
-			request: {
-				type: 'GET',
-				url: 'api/v0/nonprofits/' + id
-			}
-		});
-	}).catch( err => {
-		console.log(err);
-		res.status(500).json({error: err});
-	});
+    const id = req.params.nonprofitId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Nonprofit.update({
+        _id: id
+    }, {
+        $set: updateOps
+    }).exec().then(result => {
+        res.status(200).json({
+            message: 'Nonprofit updated',
+            request: {
+                type: 'GET',
+                url: 'api/v0/nonprofits/' + id
+            }
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
 });
 
 router.delete('/:nonprofitId', (req, res, next) => {
-	const id = req.params.nonprofitId;
-	Nonprofit.remove({_id: id})
-	.exec()
-	.then(result =>{
-		res.status(200).json({
-			message: 'Nonprofit deleted',
-			request: {
-				type: 'POST',
-				url: 'api/v0/nonprofits',
-				body: { name: 'String', url: 'String', address: 'String', url: 'String', image: 'String', 
-                logo: 'String', desc: 'String', short_desc: 'String', evaluatorId: 'String',
-                stats: {metric1: 'String', metric2: 'Number'}}
-			}
-		});
-	}).catch(err =>{
-		console.log(err);
-		res.status(500).json({error: err})
-	});
+    const id = req.params.nonprofitId;
+    Nonprofit.remove({
+            _id: id
+        })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'Nonprofit deleted',
+                request: {
+                    type: 'POST',
+                    url: 'api/v0/nonprofits',
+                    body: {
+                        name: 'String',
+                        url: 'String',
+                        address: 'String',
+                        url: 'String',
+                        image: 'String',
+                        logo: 'String',
+                        desc: 'String',
+                        short_desc: 'String',
+                        evaluatorId: 'String',
+                        stats: {
+                            metric1: 'String',
+                            metric2: 'Number'
+                        }
+                    }
+                }
+            });
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 
 module.exports = router;
